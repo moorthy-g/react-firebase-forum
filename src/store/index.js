b2b0a8f4cf29d41cbf3c4364e646d26485dd69aa
Moorthy <moorthy.g@krds.fr>
2018-01-31 19:26:23 +0530

    Configure redux dev tools

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,28 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
 import rootReducer from './rootReducer';
 
 export function configureStore() {
-  const store = window.store = createStore(rootReducer, applyMiddleware(thunk, logger));
+  let enhancers;
+
+  if (process.env.NODE_ENV !== 'production') {
+    const logger = require('redux-logger').default;
+    const composeEnhancers =
+      typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+        ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+        : compose;
+    enhancers = composeEnhancers(applyMiddleware(thunk, logger));
+  } else {
+    //prod only middlewares
+    enhancers = compose(applyMiddleware(thunk));
+  }
+
+  const store = createStore(rootReducer, enhancers);
 
   if (module.hot) {
     // Enable Webpack hot module replacement for reducers
     module.hot.accept('./rootReducer', () => {
       const nextRootReducer = require('./rootReducer').default;
-      console.log('nextRootReducer', nextRootReducer);
       store.replaceReducer(nextRootReducer);
     });
   }
