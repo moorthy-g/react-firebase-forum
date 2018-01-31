586117707b4d239c24b01ddacba3812656862ced
Moorthy <moorthy.g@krds.fr>
2018-01-31 19:48:27 +0530

    Include avatar in threads list

--- src/screens/threads/components/ThreadItem.js
+++ src/screens/threads/components/ThreadItem.js
@@ -5,11 +5,15 @@ import { truncate } from 'utils/helpers';
 import styles from '../styles.css';
 
 const ThreadItem = props => {
+  const name = props.user.first_name + ' ' + props.user.last_name;
+  const { avatar } = props.user;
   return (
     <li styleName="thread-item" className="list-group-item">
       <h4>{props.title.charAt(0).toUpperCase() + props.title.slice(1)}</h4>
       <p>{truncate(props.description)}</p>
-      <h6 styleName="user">posted by {props.user.first_name}</h6>
+      <h6 styleName="user">
+        posted by {name} <img src={avatar} alt='' />
+      </h6>
     </li>
   );
 };
@@ -17,7 +21,7 @@ const ThreadItem = props => {
 ThreadItem.propTypes = {
   title: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
-  user: PropTypes.object
+  user: PropTypes.object.isRequired
 }
 
 export default ThreadItem;
