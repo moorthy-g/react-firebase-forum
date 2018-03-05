4da524fcf09586b112d65b6ac66dbca5a14936c5
Moorthy <moorthy.g@krds.fr>
2018-03-05 20:08:40 +0530

    Clean up utils helper

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,25 +1,8 @@
-export function normalize(data) {
-  return data.reduce((accumulator, current) => {
-    accumulator[current.id] = current
-    return accumulator;
-  }, {})
-}
-
-export function fetchAndNormalize(url) {
-  return fetch(url)
-    .then(response => response.json())
-    .then(normalize);
-}
-
 export function truncate(inputString, limit = 100) {
   if(inputString.length <= limit)
     return inputString;
 
   return inputString.slice(0, limit) + '...';
-}
-
-export function copyObject(object) {
-  return Object.assign({}, object);
 }
 
 export function updateObject(...args) {
@@ -28,7 +11,7 @@ export function updateObject(...args) {
   return state;
 }
 
-export function reduceArrayToObject(objectArray){
+export function reduceArrayToObject(objectArray) {
   return objectArray.reduce((accumulator,object) => Object.assign(accumulator, object), {});
 }
 
